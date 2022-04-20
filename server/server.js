3d5d192bea7a22360a43699caca38eb2783c2512
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-04-20 11:33:55 -0500

    Fix router

--- server/server.js
+++ server/server.js
@@ -8,11 +8,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
