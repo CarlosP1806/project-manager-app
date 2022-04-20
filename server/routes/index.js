963236b1a7fbeefec4e4d210d57ae0262c6241b0
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-04-20 13:15:40 -0500

    Implement auth api

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 const path = require('path');
 
 const projectRoutes = require('./project_routes/project_routes');
+const userRoutes = require('./user_routes/user_routes');
 
 router.use('/project', projectRoutes);
+router.use('/user', userRoutes);
 
 // Serve react front-end in production
 router.use((req, res) => {
---
+++ server/routes/user_routes/user_routes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const User = require('../../models/User');
+
+// Get current user using middleware
+router.get('/me', async ({ user = null, params }, res) => {
+  try {
+    const foundUser = await User.findOne({
+      $or: [{ _id: user ? user._id : params.id }, { username: params.username }]
+    });
+    if (!foundUser) {
+      res.status(404).json({ message: 'cannot find user' });
+      return;
+    }
+    res.json(foundUser);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+// Create a new user
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    if (!user) {
+      res.status(500).json({ message: "something went wrong" });
+      return;
+    }
+    const token = null;
+    res.json({ token, user });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+// Login route
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      $or: [{ username: req.body.username }, { email: req.body.email }]
+    });
+    if (!user) {
+      res.status(404).json({ message: "cannot find user" });
+      return;
+    }
+
+    const correctPwd = req.body.password === user.password;
+    if (!correctPwd) {
+      res.status(500).json({ message: "invalid credentials" });
+      return;
+    }
+    const token = null;
+    res.json({ token, user });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+module.exports = router;
